4bf085d8c3e75e76cc67786f4cf5f8df43402ab0
Jorgosz Diamantisz <Jorgosz.Diamantisz1@hu.ibm.com>
2022-10-01 13:22:10 +0200

    list movies, dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,34 @@
 let express = require('express');
 let router = express.Router();
 const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const apiKey='';
-const apiBaseUrl = 'http://api.themoviedb.org/3';
+const apiKey = process?.env?.API_KEY;
+const apiBaseUrl = process?.env?.API_BASE_URL;
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
-//const imageBaseUrl = 'http://image.tmdb.org/t/p/w300'
+const imageBasedUrl = process?.env?.IMAGE_BASE_URL;
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  try {
-    const response = await axios.get(nowPlayingUrl);
-  } catch (e) {
-    console.log(e)
-  }
-  res.render('index', );
+
+router.use((req, res, next) => {
+    res.locals.imageBaseUrl = imageBasedUrl;
+    next()
+})
+router.get('/', async function (req, res, next) {
+    try {
+        const movies = await axios.get(nowPlayingUrl);
+        res.render('index', {parsedData: movies.data.results});
+    } catch (e) {
+    }
 });
 
+router.get('/movie/:id',async (req,res,next)=>{
+    const movieUrl=`${apiBaseUrl}/movie/${req.params.id}?api_key=${apiKey}`;
+    console.log(movieUrl)
+    const movies = await axios.get(movieUrl);
+    //res.json(raw.data);
+    console.log(movies)
+    res.render('single-movie', {parsedData: movies?.data});
+});
 module.exports = router;
